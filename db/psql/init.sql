1ba0c277283702e3ec83f7f1fd2acea4acccc299
hongroklim <hongrr123@gmail.com>
2021-11-30 11:46:47 +0900

    Updated Schema

--- db/psql/init.sql
+++ db/psql/init.sql
@@ -13,14 +13,14 @@ ALTER ROLE usr_recm8 SET search_path = recm8;
 CREATE TABLE recm8.image (
   img_id SERIAL PRIMARY KEY,
   image bytea,
-  elapsed_sec float,
+  msec int,
   created_at timestamp DEFAULT (now())
 );
 
 CREATE TABLE recm8.detection (
   det_id SERIAL PRIMARY KEY,
   img_id int,
-  obj_id int,
+  obj_name varchar,
   score float,
   x1 float,
   y1 float,
@@ -29,20 +29,19 @@ CREATE TABLE recm8.detection (
 );
 
 CREATE TABLE recm8.category (
-  obj_id SERIAL PRIMARY KEY,
+  obj_name varchar PRIMARY KEY,
   sup_id int,
-  name varchar,
   special_instruct varchar
 );
 
 CREATE TABLE recm8.super_category (
   sup_id SERIAL PRIMARY KEY,
-  name varchar,
+  sup_name varchar,
   is_recyclable bool
 );
 
 ALTER TABLE recm8.detection ADD FOREIGN KEY (img_id) REFERENCES recm8.image (img_id);
 
-ALTER TABLE recm8.detection ADD FOREIGN KEY (obj_id) REFERENCES recm8.category (obj_id);
+ALTER TABLE recm8.detection ADD FOREIGN KEY (obj_name) REFERENCES recm8.category (obj_name);
 
 ALTER TABLE recm8.category ADD FOREIGN KEY (sup_id) REFERENCES recm8.super_category (sup_id);
